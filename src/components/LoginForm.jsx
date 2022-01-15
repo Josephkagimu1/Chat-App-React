d1f012f74268617b73bda5ee8c5da01db02abee4
Josephkagimu1 <88326256+Josephkagimu1@users.noreply.github.com>
2022-01-15 08:08:22 -0800

    Update LoginForm.jsx
    Changed the form title to red

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -29,7 +29,7 @@ const Modal = () => {
   return (
     <div className="wrapper">
       <div className="form">
-        <h1 className="title">Chat Application</h1>
+        <h1 className="title" style={{color: 'red'}}>Chat Application</h1>
         <form onSubmit={handleSubmit}>
           <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="input" placeholder="Username" required />
           <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="input" placeholder="Password" required />
