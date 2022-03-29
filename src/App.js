0a0c42d14264a6160f98523c8f2cda2371eafd0b
Josephkagimu1 <kagimujoseph00@gmail.com>
2022-03-29 13:25:02 -0700

    add to

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
+import {useState} from 'react';
 import './App.css';
 import { ChatEngine } from 'react-chat-engine';
 import ChatFeed from './components/ChatFeed';
 import LoginForm from './components/LoginForm';
 
+// dark-mode start
+import styled, { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme, GlobalStyles } from "./components/themes.js";
+
+const StyledApp = styled.div`
+  color: ${(props) => props.theme.fontColor};
+`;
 
 const App = () => {
+	const [theme, setTheme] = useState("light");
+	const themeToggler = () => {
+		theme === "light" ? setTheme("dark") : setTheme("light");
+	  };
+
     if(!localStorage.getItem('username')) return <LoginForm />
 
 	return (
+		<ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+			<GlobalStyles />
+			<StyledApp>
+        		<button onClick={() => themeToggler()}>Change Theme</button>
+     		 
 		<ChatEngine
 			height='100vh'
 			userName='jose'
@@ -15,6 +33,9 @@ const App = () => {
 			projectID='fdeff685-b143-4839-80e8-f4667642893b'
             renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} /> }
 		/>
+
+ 			</StyledApp>
+		</ThemeProvider>
 	);
 }
 
